3544d29e3d3108e479f2d0bd385d3df9002f566b
Kenneth Clausen <me@kjc.dk>
2022-12-27 17:41:50 +0100

    deploy

--- assets/HomeView-6a2d9642.js
+++
@@ -1,1 +0,0 @@
-import{j as e,F as i,a as n,L as o}from"./index-15392527.js";function r(){return e(i,{children:["HomeView",n(o,{to:"/login",children:"Login"})]})}export{r as default};
---
+++ assets/HomeView-a99629ba.js
@@ -0,0 +1,1 @@
+import{j as e,F as r}from"./index-d4614cd5.js";function a(){return e(r,{})}export{a as default};
